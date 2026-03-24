5bfdefcebe634427b2dab158c507264576f07543
Haider Ali <haideralyy01@gmail.com>
2026-03-24 17:57:32 +0530

    feat(middleware): add user middleware

---
+++ backend/configurations/config.js
@@ -0,0 +1,4 @@
+const JWT_SECRET = process.env.JWT_SECRET;
+module.exports = {
+    JWT_SECRET
+}
---
+++ backend/middlewares/user.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require('../configurations/config');
+
+function middleware(req, res, next) {
+    try {
+        const token = req.headers.token;
+        if (!token) {
+            return  res.status(401).json({
+                message: "No token provided"
+            });
+        }
+
+        const decodedData = jwt.verify(token, JWT_SECRET);
+        req.userId = decodedData.id;
+        next();
+    } catch (err) {
+        res.status(401).json({
+            message: "Invalid token",
+            error: err.message
+        });
+    }
+}
+
+module.exports = {
+    middleware
+}
