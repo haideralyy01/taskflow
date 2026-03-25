0429058f6bdf5158a4075fd25b55e10047ca3f42
Haider Ali <haideralyy01@gmail.com>
2026-03-25 22:30:04 +0530

    chore(server): enable CORS for API requests

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const { userRouter } = require("./routes/user");
 const { todoRouter } = require("./routes/todo");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST","PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}))
 
 app.use("/api", userRouter);
 app.use("/api", todoRouter);
